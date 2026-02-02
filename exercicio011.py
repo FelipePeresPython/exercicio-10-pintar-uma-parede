d6efec1a531f86fa8bfdebb1215ca4b41fa77bb0
FelipePython <felipehypixel10@gmail.com>
2026-02-02 20:00:19 -0300

    Add files via upload

---
+++ exercicio011.py
@@ -0,0 +1,18 @@
+print("vamos medir um quarto")
+print()
+print("qual sera a largura da parede?")
+L=int(input(""))
+print()
+print("agora a altura da parede")
+A=int(input(""))
+print("")
+print("agora vamos medir a area")
+print()
+AA=L*A
+print(f"area é altura x largura,então será {L} vezes {A} que vai dar: {AA} a area")
+print()
+print("<agora vamos pintar o quarto> \n sabendo que cada litro de tinta pintaria 2m**2 \n quantos litros de tinta precisaria pra pintar o quarto todo?")
+T=AA/2
+print()
+print(f"precisaria de {T}litros de tinta")
+print()
